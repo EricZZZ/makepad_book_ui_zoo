ef4caea3b4a7e61531c53c9182ff0ea1beff5287
EricZzz <biggogten@gmail.com>
2025-04-22 10:19:57 +0800

    feat: init makepad ui project

---
+++ src/app.rs
@@ -0,0 +1,149 @@
+use makepad_widgets::*;
+
+live_design! {
+    // import Makepad theme and shaders, 以及 widgets
+    use link::theme::*;
+    use link::shaders::*;
+    use link::widgets::*;
+
+    ZooTitle = <View> {
+        width: Fill,
+        height: Fit,
+        margin: 10.0,
+        title = <H2> {
+            text: "Makepad UI Zoo"
+        }
+    }
+
+    ZooHeader = <View> {
+        width: Fill,
+        height: Fit,
+        flow: Down,
+        spacing: 10.,
+        margin: {
+            top: 9,
+            bottom: 9,
+            left: 9,
+            right: 9
+        }
+        divider = <Hr>{ }
+        title = <H3> { }
+    },
+
+    ZooDesc = <P> { },
+
+    App = {{App}} {
+        ui: <Window> {
+            show_bg: true,
+            draw_bg: {
+                // color: #970707    // dark red
+                // color: vec3(0.592, 0.027, 0.027) // dark red as above
+                // color: vec3(1, 1, 0) // yellow
+                // color: #000 // black
+                }
+            window: {
+                title: "Makepad UI zoo"
+            },
+            caption_bar = {
+                visible: true,
+                margin: {left: -500},
+                caption_label = { label = {text: "Makepad book UI Zoo caption bar"} },
+            },
+
+            body = <View> {
+                width: Fill, height: Fill,
+                flow: Down, //child components will be arranged vertically
+                spacing: 10.,   //spacing between child components
+                margin: 0.,
+                scroll_bars: <ScrollBars> {}     //margin around the component
+
+                <ZooTitle> {}
+
+                <ZooHeader> {
+                    title = {text: "Intro"}
+                    <ZooDesc> {
+                        text: "Intro."
+                    }
+                    <View> {
+                        width: Fill, height: Fit,
+                        flow: Down,
+                        <P> { text: "- Shader-based: what does that mean for how things work." }
+                        <P> { text: "- Inheritance mechanisms in the DSL." }
+                        <P> { text: "- Introduction to the layout system." }
+                        <P> { text: "- Base theme parameters." }
+                        <P> { text: "- Typographic system. Base font-size and contrast." }
+                        <P> { text: "- Space constants to control denseness of the design." }
+                        <P> { text: "- Transparency mechanism of the widgets. Nesting for structure." }
+                    }
+                }
+
+                <ZooHeader> {
+                    title = {text: "Control Heights & Text Baselines"}
+                    <ZooDesc> {
+                        text: "Control heights and text baselines"
+                    }
+                    <View> {
+                        width: Fill, height: Fit,
+                        align: { x: 0., y: 0.}
+                        flow: Right,
+                        spacing: (THEME_SPACE_2)
+                        <P> { text: "TestLabel", width: Fit}
+                        <Vr> {}
+                        <LinkLabel> { text: "TestButton", width: Fit}
+                        <CheckBox> { text: "TestButton"}
+                        <CheckBoxToggle> { text: "TestButton"}
+                        <ButtonFlat> { text: "TestButton"}
+                        <Button> { text: "TestButton"}
+                        <TextInput> { text: "TestButton"}
+                        <Slider> { text: "TestButton"}
+                        <SliderBig> { text: "TestButton"}
+                    }
+                }
+
+                <ZooHeader> {
+                    title = {text: "Typography"}
+                    <ZooDesc> {
+                        text: "Typography."
+                    }
+                    <View> {
+                        width: Fill, height: Fit,
+                        flow: Down,
+                        <H1> { text: "H1 headline" }
+                        <H1italic> { text: "H1 italic headline" }
+                        <H2> { text: "H2 headline" }
+                        <H2italic> { text: "H2 italic headline" }
+                        <H3> { text: "H3 headline" }
+                        <H3italic> { text: "H3 italic headline" }
+                        <H4> { text: "H4 headline" }
+                        <H4italic> { text: "H4 italic headline" }
+                        <P> { text: "P copy text" }
+                        <Pitalic> { text: "P italic copy text" }
+                        <Pbold> { text: "P bold copy text" }
+                        <Pbolditalic> { text: "P bold italic copy text" }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Live, LiveHook)]
+pub struct App {
+    #[live]
+    ui: WidgetRef,
+}
+
+impl LiveRegister for App {
+    fn live_register(cx: &mut Cx) {
+        // Register Makepad Widgets' live design
+        makepad_widgets::live_design(cx);
+    }
+}
+
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.ui.handle_event(cx, event, &mut Scope::empty());
+    }
+}
+
+app_main!(App);
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    makepad_book_ui_zoo::app::app_main();
+}
